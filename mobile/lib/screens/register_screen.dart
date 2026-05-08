d13d85bf3c20f37c5120ddba44ee779d0d6f1060
akramelassri <168238434+akramelassri@users.noreply.github.com>
2026-05-08 17:12:15 +0100

    Switch to a stateful widget and add a form

--- mobile/lib/screens/register_screen.dart
+++ mobile/lib/screens/register_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   @override
+  _RegisterState createState() {
+    return _RegisterState();
+  }
+}
+
+class _RegisterState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('hello')));
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'Full Name'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your full name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    print('test');
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
