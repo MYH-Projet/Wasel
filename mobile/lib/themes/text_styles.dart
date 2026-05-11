04c341b2ba31e8d8b0b10153bfb469ef93c26dae
akramelassri <168238434+akramelassri@users.noreply.github.com>
2026-05-11 21:04:08 +0100

    Refactor text styles

--- mobile/lib/themes/text_styles.dart
+++ mobile/lib/themes/text_styles.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-final displayText = GoogleFonts.inter(
-  textStyle: TextStyle(fontSize: 32, fontWeight: FontWeight(700), height: 1.2),
+final _base = GoogleFonts.inter();
+
+final displayText = _base.copyWith(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+  height: 1.2,
 );
 
-final headingText = GoogleFonts.inter(
-  textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight(600), height: 1.3),
+final headingText = _base.copyWith(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+  height: 1.3,
 );
 
-final subHeadingText = GoogleFonts.inter(
-  textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight(500), height: 1.4),
+final subHeadingText = _base.copyWith(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  height: 1.4,
 );
 
-final bodyText = GoogleFonts.inter(
-  textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight(400), height: 1.5),
+final bodyText = _base.copyWith(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  height: 1.5,
 );
 
-final labelText = GoogleFonts.inter(
-  textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight(500), height: 1.4),
+final labelText = _base.copyWith(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  height: 1.4,
 );
 
 // For buttons
-final bolderLabelText = GoogleFonts.inter(
-  textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight(600), height: 1.4),
+final bolderLabelText = _base.copyWith(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  height: 1.4,
 );
 
-final captionText = GoogleFonts.inter(
-  textStyle: TextStyle(fontSize: 12, fontWeight: FontWeight(400), height: 1.5),
+final captionText = _base.copyWith(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  height: 1.5,
 );
